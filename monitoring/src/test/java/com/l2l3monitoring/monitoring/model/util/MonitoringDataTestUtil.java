8a65f250dbd475e11e5eb3e79148b6bac50f9857
szogi <szogibalu@gmail.com>
2015-07-12 17:59:42 +0200

    M6 Added service test

---
+++ monitoring/src/test/java/com/l2l3monitoring/monitoring/model/util/MonitoringDataTestUtil.java
@@ -0,0 +1,25 @@
+package com.l2l3monitoring.monitoring.model.util;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import com.l2l3monitoring.monitoring.model.MonitoringData;
+
+public final class MonitoringDataTestUtil {
+
+    private MonitoringDataTestUtil() {
+    }
+
+    public static MonitoringData createSampleModel() {
+	return MonitoringData.builder()
+		.withClass(Object.class)
+		.withMethodName("calculateSomething")
+		.addParameter("value", BigDecimal.ONE)
+		.withReturnValue("Hello World!")
+		.withExecutionTime(Duration.ofSeconds(60))
+		.withTimestamp(LocalDateTime.of(2015, 7, 10, 21, 34))
+		.build();
+    }
+
+}
